8311d7a90cdd1ad52dca6f306a0f587e77c7f843
aibayanyu20 <aibayanyu@qq.com>
2025-10-30 11:30:33 +0800

    feat: add fullscreen

---
+++ playground/src/pages/spin/demo/custom-indicator.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { LoadingOutlined } from '@antdv-next/icons'
+</script>
+
+<template>
+  <a-flex align="center" gap="middle">
+    <a-spin size="small">
+      <template #indicator>
+        <LoadingOutlined spin />
+      </template>
+    </a-spin>
+    <a-spin>
+      <template #indicator>
+        <LoadingOutlined spin />
+      </template>
+    </a-spin>
+    <a-spin size="large">
+      <template #indicator>
+        <LoadingOutlined spin />
+      </template>
+    </a-spin>
+    <a-spin>
+      <template #indicator>
+        <LoadingOutlined spin style="font-size: 48px" />
+      </template>
+    </a-spin>
+  </a-flex>
+</template>
