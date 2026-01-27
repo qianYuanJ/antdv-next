bf9a81adf95f2930ec8e400c3a7db8a10ff0a8d9
Bin <fuwb0407@gmail.com>
2026-01-27 22:08:26 +0800

    feat(docs): switch text to bash-style md block

--- playground/src/components/install-dependencies/source-code.vue
+++ playground/src/components/install-dependencies/source-code.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import { useClipboard } from '@vueuse/core'
+import { computed } from 'vue'
+
 defineOptions({ name: 'SourceCode' })
 const props = defineProps<{ value?: string }>()
+
+const { copied, copy } = useClipboard({
+  source: computed(() => props.value ?? ''),
+  legacy: true,
+})
 </script>
 
 <template>
-  <a-alert :styles="{ root: { backgroundColor: 'var(--ant-color-fill-tertiary)', border: 'none' } }" type="info" :show-icon="false" :title="props.value" />
+  <div class="language-bash">
+    <button class="copy" :class="{ copied }" @click="copy()" />
+    <span class="lang">bash</span>
+    <pre><code>{{ props.value }}</code></pre>
+  </div>
 </template>
